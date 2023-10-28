84fcea758e460dfe7fbe819cda91e30d39015bd3
Hmxs <18321938171@163.com>
2023-10-28 08:53:16 +0800

    complete normal enemy - 202310280853

--- Assets/Scripts/Enemy/EnemyNormal.cs
+++ Assets/Scripts/Enemy/EnemyNormal.cs
@@ -1,17 +1,36 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace Enemy
 {
     public class EnemyNormal : Enemy
     {
+        [Title("没被吸时")]
+        public float normalAcceleration;
+        public float normalMaxSpeed;
+
+        [Title("被吸时")]
+        public float suckedAcceleration;
+        public float suckedMaxSpeed;
+
         protected override void NotBeingSuckedIn()
         {
-            Debug.Log("Not Being Sucked In");
+            var direction = ((Vector2)(transform.position - Player.position)).normalized;
+            if (Rb.velocity.magnitude < normalMaxSpeed ||
+                Rb.velocity.x * direction.x < 0 || Rb.velocity.y * direction.y < 0)
+            {
+                Rb.AddForce(direction * normalAcceleration);
+            }
         }
 
         protected override void BeingSuckedIn()
         {
-            Debug.Log("Is Being Sucked In");
+            var direction = ((Vector2)(Player.position - transform.position)).normalized;
+            if (Rb.velocity.magnitude < suckedMaxSpeed ||
+                Rb.velocity.x * direction.x < 0 || Rb.velocity.y * direction.y < 0)
+            {
+                Rb.AddForce(direction * suckedAcceleration);
+            }
         }
 
         protected override void Died()
